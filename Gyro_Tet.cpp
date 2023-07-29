4e3e7a65ff1f415320cb67433c43aa895b76d4c7
Lennard Karger <lennard.karger.95@gmail.com>
2023-07-29 14:49:20 +0200

    debug stuff

--- Gyro_Tet.cpp
+++ Gyro_Tet.cpp
@@ -16,26 +16,26 @@
 
 
 MPU6050 mpu6050;
-
+int here =0;
 int main()
 {
     stdio_init_all();
 
-    uint8_t error=mpu6050.begin();
+    uint8_t error=mpu6050.begin(0,Max2g,Max250Dps,Max260Hz,Div7,I2C_PORT);
     printf(mpu6050.error_str(error));
 
     sleep_ms(5000);
 
     while(1){
-    
-        mpu6050.absent();
+        here = 0;
+        here = mpu6050.absent();
+        printf("read status: %d\n",here);
         MPU6050_t data= mpu6050.get();
         
         printf("ae= %d, ax= %3.1f, ay= %3.1f, az= %3.1f\n",data.accel.error,data.accel.x,data.accel.y,data.accel.z);
         printf("ge= %d, gx= %3.1f, gy= %3.1f, gz= %3.1f\n",data.gyro.error,data.gyro.x,data.gyro.y,data.gyro.z);
         printf("de= %d, dx= %3.1f, dy= %3.1f, dz= %3.1f\n",data.dir.error,data.dir.roll,data.dir.pitch,data.dir.yaw);
         printf("te= %d, tt= %3.1f\n\n",data.temp.error,data.temp.t);
-        
         sleep_ms(1000);
     }
     
